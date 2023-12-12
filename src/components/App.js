eacba2b6ed29bd987f36cb86643146001c01ad84
Shubham Jain <shubham.rapariya2@gmail.com>
2023-12-12 17:36:37 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+
+  const [inputText,inputTextSetter] = useState('');
+
+  function inputHandler(e) {
+    let valueText = "Hello! " + e.target.value
+    inputTextSetter(valueText)
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h3>Enter Your Name:</h3>
+        <input type="text" name="name" id="name" onKeyUp={inputHandler} />
+        <p>{inputText}</p>
     </div>
   )
 }
